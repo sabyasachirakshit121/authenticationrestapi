57818dfa1a8eb26e7bbeccaed7ac8a89888df8c5
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2022-12-08 17:26:43 +0530

    user serializer in views added

--- first/views.py
+++ first/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
+
+from . import UserSerializer
 # Create your views here.
+
+
+class UserSerializer(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated)
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
